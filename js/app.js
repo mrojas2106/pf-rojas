3c1139933acb1b9514afafb76885e133c1465c9e
Maximiliano Rojas <maximrojas21@hotmail.com>
2022-09-16 16:47:12 -0300

    Se realizaron cambios en el color de la web, se agrego el menu hamburguesa con JS propio

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".menu-hamburguesa");
+const closeMenuBtn = document.querySelector(".general-conteiner");
+
+function abrirMenu() {
+  menu.classList.add("menu-active");
+}
+
+function cerrarMenu() {
+  menu.classList.remove("menu-active")
+}
+
+openMenuBtn.addEventListener("click", abrirMenu);
+closeMenuBtn.addEventListener("click", cerrarMenu);
+
+
+
