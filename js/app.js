921ca245fbe356f66c6476321a66f8a94134340c
Maximiliano Rojas <maximrojas21@hotmail.com>
2022-09-17 17:23:24 -0300

    Se cambia comportamiento del boton y el padding del footer centrando los elementos

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ const closeMenuBtn = document.querySelector(".general-conteiner");
 
 function abrirMenu() {
   menu.classList.add("menu-active");
+  openMenuBtn.classList.add("noMostrar")
 }
 
 function cerrarMenu() {
   menu.classList.remove("menu-active")
+  openMenuBtn.classList.remove("noMostrar")
 }
 
 openMenuBtn.addEventListener("click", abrirMenu);
