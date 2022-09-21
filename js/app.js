1537c609f762a267b0f59dcb7e3c97c67051c669
Maximiliano Rojas <maximrojas21@hotmail.com>
2022-09-21 18:52:37 -0300

    Se agegaron efectos a los titulos y al bajar el Scroll

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".menu-hamburguesa");
 const closeMenuBtn = document.querySelector(".general-conteiner");
+const apareceTitulo = document.querySelector(".efectoDesaparecer-no");
+const boxes = document.querySelectorAll(".tarjeta");
 
 function abrirMenu() {
   menu.classList.add("menu-active");
@@ -15,5 +17,40 @@ function cerrarMenu() {
 openMenuBtn.addEventListener("click", abrirMenu);
 closeMenuBtn.addEventListener("click", cerrarMenu);
 
+window.onscroll = function(){
+
+  scroll = document.documentElement.scrollTop;
+
+  if (scroll >= 130){
+    apareceTitulo.classList.add("efectoDesaparecer-si");
+    apareceTitulo.classList.add("titulo-position");
+  } 
+
+ if (scroll >=280) {
+  for (const box of boxes) {
+    box.classList.add("efectoDesaparecer-si");
+  }
+} 
+
+}
+
+if (window.matchMedia("(max-width: 450px)").matches) {
+  window.onscroll = function(){
+
+    scroll = document.documentElement.scrollTop;
+  
+    if (scroll >= 350){
+      apareceTitulo.classList.add("efectoDesaparecer-si");
+      apareceTitulo.classList.add("titulo-position");
+    } 
+  
+   if (scroll >=480) {
+    for (const box of boxes) {
+      box.classList.add("efectoDesaparecer-si");
+    }
+  } 
+  
+  }
+} 
 
 
